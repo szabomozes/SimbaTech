277bd94f10ec3e11bcd82eb1ffd83b0ba21cf752
Isztl Dominik <isztl.domi@gmail.com>
2025-03-17 09:57:44 +0100

    commit

--- SimbaTech1.0/src/panels/game/EventPanel.java
+++ SimbaTech1.0/src/panels/game/EventPanel.java
@@ -25,6 +25,7 @@ public class EventPanel extends JPanel {
         calendar = new Calendar(getWidth());
 
         add(logoutButton);
+
         add(calendar);
 
         addMouseListener(new MouseAdapter() {
