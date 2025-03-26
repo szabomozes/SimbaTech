e62db5afef98f8e51b133f2d5441009e159d2b04
Isztl Dominik <vru6fs@inf.elte.hu>
2025-03-26 18:57:43 +0000

    Delete

--- SimbaTech1.0/src/Main.java
+++
@@ -1,9 +0,0 @@
-import core.Resources;
-import panels.Window;
-
-public class Main {
-    public static void main(String[] args) {
-        Resources.Instance.load();
-        new Window();
-    }
-}
--- SimbaTech1.0/src/logic/Speed.java
+++
@@ -1,9 +0,0 @@
-package logic;
-
-public class Speed{
-
-    public static Speed Instance = new Speed();
-
-    public SpeedEnum speedEnum = SpeedEnum.SNAIL;
-
-}
